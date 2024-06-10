8671da932a5c6b35dacdd029d0109835b3261d5d
이연승 <leeys951006@gmail.com>
2024-06-10 18:04:31 +0900

    app.js 수정

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const http = require('http');
 const fs = require('fs');
 const qs = require('node:querystring');
 // const stress = require("./stress");
-const b = `<!DOCTYPE html>
+
+// * 기본 양식
+const workContent = `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
@@ -17,42 +19,35 @@ const b = `<!DOCTYPE html>
 </body>
 </html>`;
 
+// * 서버
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
     if (req.url === '/') {
       const first = fs.readFileSync('./public/index.html', 'utf8');
 
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html; charset=uft-8');
-      res.write(first);
-      res.end();
-    }
-
-    if (req.url === '/index.html') {
-      const index1 = fs.readFileSync('./public/index.html', 'utf8');
-
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html; charset=uft-8');
-      res.write(index1);
-      res.end();
+      res.writeHead(200, { 'Content-Type': 'text/html; charset=uft-8' });
+      res.end(first);
     }
 
     if (req.url === '/index.css') {
       const index2 = fs.readFileSync('./public/index.css', 'utf8');
 
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/css; charset=uft-8');
-      res.write(index2);
-      res.end();
+      res.writeHead(200, { 'Content-Type': 'text/css; charset=uft-8' });
+      res.end(index2);
     }
 
     if (req.url === '/work.html') {
       const work = fs.readFileSync('./public/work.html', 'utf8');
 
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html; charset=uft-8');
-      res.write(work);
-      res.end();
+      res.writeHead(200, { 'Content-Type': 'text/html; charset=uft-8' });
+      res.end(work);
+    }
+
+    if (req.url === '/public/work.html') {
+      const work = fs.readFileSync('./public/work.html', 'utf8');
+
+      res.writeHead(200, { 'Content-Type': 'text/html; charset=uft-8' });
+      res.end(work);
     }
 
     console.log(req.url);
@@ -67,75 +62,30 @@ const server = http.createServer((req, res) => {
         let title = a.title;
         let content = a.content;
 
-        // let filedata = fs.readdir(`./data`, 'utf-8', (err, fileList) => {
-        //   if (err) {
-        //     console.log(err);
-        //   } else {
-        //     console.log(fileList);
-        //   }
-        // });
+        let addTitle = workContent.replace('<h1></h1>', `<h1>${title}</h1>`);
+        console.log(addTitle);
+        let addContent = addTitle.replace('<div></div>', `<div>${content}</div>`);
 
-        // let c = `<!DOCTYPE html>
-        // <html lang="en">
-        // <head>
-        //   <meta charset="UTF-8">
-        //   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        //   <title>Work</title>
-        // </head>
-        // <body>
-        //   <form action="/submit" method="POST">
-        //     <label for="title">제목 :</label><br>
-        //     <input type="text" id="title" name="title"><br><br>
-        //     <label for="content">내용 :</label><br>
-        //     <textarea name="content" id="content"></textarea><br><br>
-        //     <input type="submit" value="submit">
-        //   </form>
-
-        //   <h1>Stress 내역</h1>
-
-        //   <ul id=list>
-        //     <li>${file}</li>
-        //   </ul>
-
-        //   </script>
-
-        // </body>
-        // </html>`;
-
-        fs.writeFile(`./data/${title}.html`, b, (err) => {
+        fs.writeFile(`./data/${title}.html`, addContent, (err) => {
           if (err) {
             console.log('Error');
             return;
           }
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'text/html; charset=utf-8');
-          res.end(b);
+          res.writeHead(200, { 'Content-Type': 'text/html; charset=uft-8' });
+          res.end(addContent);
         });
-
-        // fs.writeFile(`./data/work2.html`, c, (err) => {
-        //   if (err) {
-        //     console.log('Error');
-        //     return;
-        //   }
-        //   res.statusCode = 200;
-        //   res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        //   res.end(b);
-        // });
-
-        // fs.writeFile('./public/work.html', c, (err) => {
-        //   if (err) {
-        //     console.log('Error');
-        //     return;
-        //   }
-        //   res.statusCode = 200;
-        //   res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        //   res.end(b);
-        // });
       });
     }
   }
 });
 
+// let filedata = fs.readdir(`./data`, 'utf-8', (err, fileList) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(fileList);
+//   }
+// });
 const PORT = 3000;
 server.listen(PORT, function (err) {
   if (err) {
