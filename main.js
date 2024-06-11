38a1fbd13b5a1c6400d4d7a6175c9f20ffeb944e
이연승 <leeys951006@gmail.com>
2024-06-11 21:13:47 +0900

    중간저장

--- main.js
+++ main.js
@@ -24,14 +24,24 @@ const main = {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>a</title>
   </head>
   <body>
     <h1></h1>
     <div></div>
-    <div><a href="./public/work.html">돌아가기</a></div>
+    <div><a href="/work.html">돌아가기</a></div>
+
+    <div id="root"></div>
+
+    <form action="/contentSubmit" method="POST">
+        <label for="content">댓글 작성 :</label><br />
+        <textarea name="content" id="content"></textarea><br /><br />
+        <input type="submit" value="submit" />
+      </form>
   </body>
   </html>`,
+
+  contentSub: `<p></p>`
 }
 
 module.exports = main
